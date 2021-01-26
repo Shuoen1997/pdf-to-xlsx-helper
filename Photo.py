34a082e4bcf29a220df27c846b47bc8ff5e159ae
Shuo-En Li <sli112@ucsc.edu>
2021-01-25 23:48:36 -0800

    Finally got it workinggaa

--- Photo.py
+++
@@ -1,8 +0,0 @@
-class Photo(object):
-
-    def __init__(self, filename, user):
-        self.file_name = filename
-        self.user = user
-
-    def store(self):
-        print("Stored")
--- app.py
+++ app.py
@@ -1,38 +1,53 @@
-from flask import Flask, request, redirect, url_for, flash, render_template, send_file
-from flask_uploads import (UploadSet, configure_uploads, IMAGES,
-                              UploadNotAllowed)
-from Photo import Photo
+from flask import Flask, request, render_template, send_from_directory, url_for, redirect, flash
+import os
+from werkzeug.utils import secure_filename
+from pdf_convertor import *
+
 app = Flask(__name__)
-photos = UploadSet('photos', IMAGES)
+
+UPLOAD_FOLDER = os.getcwd() + '/pdf_folder'
+DOWNLOAD_FOLDER = os.getcwd() + '/xlsx_folder'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['DOWNLOAD_FOLDER'] = DOWNLOAD_FOLDER
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/', methods=['POST', 'GET'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            print("No file part")
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            return redirect(request.url)
+        if file:
+            filename = secure_filename(file.filename)
+            full_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(full_path)
+
+            extract_PDF_textbox(pdf_name=full_path)
+            output_file_name = convert_to_xlsx(pdf_name=filename)
+            file.save(output_file_name)
+            print(output_file_name)
+            try:
+                return redirect(url_for('download', filename=output_file_name))
+            except Exception:
+                abort(404)
+    return render_template('index.html')
+
+@app.route('/convert/<filename>')
+def convert(filename):
 
 
-@app.route('/upload', methods=['GET', 'POST'])
-def upload():
-    if request.method == 'POST' and 'photo' in request.files:
-        global photos
-        filename = photos.save(request.files['photo'])
-        rec = Photo(filename=filename, user=g.user.id)
-        rec.store()
-        flash("Photo saved.")
-        return None
-    if request.method == 'POST' and 'download' in request.form:
-        return redirect(url_for('download'))
-
-    return render_template('main.html')
-
-@app.route('/download')
-def download():
-    print("Downloading...")
-    return send_file('pdf_folder/2021012000999.pdf',
-                     mimetype='application/pdf',
-                     attachment_filename='myPDF.pdf',
-                     as_attachment=True)
+@app.route('/download/<filename>')
+def download(filename):
+    return send_from_directory(app.config['DOWNLOAD_FOLDER'],
+                               filename=filename,
+                               mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    app.config['SESSION_TYPE'] = 'filesystem'
+    print(UPLOAD_FOLDER)
+    print(DOWNLOAD_FOLDER)
     app.run(debug=True)
